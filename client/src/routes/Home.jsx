51bf87baeba95e3aa1417f71984d14a7f4fd9578
0reilly <oreilly.119@osu.edu>
2022-04-24 18:52:29 -0400

    forgot import

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from "../components/Header"
 import JobList from '../components/JobList'
 import Email from '../components/Email'
+import VisitorCount from "../components/VisitorCount";
 
 const Home = () => {
     return (
